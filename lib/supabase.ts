34b69f173697cedceec50c64ebce2ed2f7aa4949
Belvestre, John Robin O. <johnrobinbelvestre14@gmail.com>
2026-09-24 17:24:33 +0800

    Updated

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL='https://uohrgnjqaqaxwfuzoiwx.supabase.co';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY='sb_publishable_sfrwwTVnx7MhwsmKQ-CXyQ_Nlb2HlM0'
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://uohrgnjqaqaxwfuzoiwx.supabase.co';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_sfrwwTVnx7MhwsmKQ-CXyQ_Nlb2HlM0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
